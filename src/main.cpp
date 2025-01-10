2ebc2aef9fce097c03e96e615de032ef876a6ea9
votre_francoisrubio_github <votre_fran31r@gmail.com_github>
2025-01-10 10:03:22 +0000

    choix utilisateur

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main ()  { 
+
+//variable permettant de compter le nombre de places disponibles
+  int nbrePlaces = 10;
+  //variable permettant de récupérer le choix de l'utilisateur
+  int choix;
+  cout << "Quel est votre choix ?" Evaluation 2 "Vous avez chisi le choix"<< endl << endl;
+  // partie à compléter à partir du 3.1
+
+
+
+  return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+}
