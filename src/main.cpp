e8218014b08d2ace24118bfd9a99d9170fab0ca8
francoisrubio_github <fran31r@gmail.com_github>
2025-01-10 10:28:48 +0000

    ajout du menu

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,14 @@ int main ()  {
   int nbrePlaces = 10;
   //variable permettant de récupérer le choix de l'utilisateur
   int choix;
-  cout << "Quel est votre choix ?" Evaluation 2 "Vous avez chisi le choix"<< endl << endl;
+  cout << "Quel places disponible voulez vous choisir ? Evaluation 2 Vous avez choisi la place "<< endl << endl;
   // partie à compléter à partir du 3.1
 
-
+  std::cout << "********   Evaluation 2    *********\n\n";
+  std::cout << " 1 - Entrée dans le parking\n\n";
+  std::cout << " 2 - Nombre de places disponibles\n\n";
+  std::cout << " 3 - Sortir du parking\n\n";
+  std::cout << " 4 - Quitter\n\n";
 
   return 0;
 
@@ -25,4 +29,3 @@ int main ()  {
 
 
 
-}
